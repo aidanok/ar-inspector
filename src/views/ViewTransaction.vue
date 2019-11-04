26c0f4d29b620af161c63b79f9bdfa1a612d2baa
aokisok <sevenclickok@gmail.com>
2019-11-04 22:19:55 +0000

    fix and cleanup txid editing

--- src/views/ViewTransaction.vue
+++ src/views/ViewTransaction.vue
@@ -3,8 +3,8 @@
     
     <div class="view-transaction-txid-jump">
       <label for="txIdInput"> View Tx </label>
-      <input ref="txIdInput" type="text" v-model="txId" v-tooltip="'Enter a Tx id to view'" @focus="$event.target.select()">
-      <a :href="`https://arweave.net/${txId}`" target="_blank">
+      <input ref="txIdInput" type="text" v-model="editingId" v-tooltip="'Enter a Tx id to view'" @focus="$event.target.select()">
+      <a :href="`https://arweave.net/${editingId}`" target="_blank">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M20 22H4a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1zm-1-2V4H5v16h14zM7 6h4v4H7V6zm0 6h10v2H7v-2zm0 4h10v2H7v-2zm6-9h4v2h-4V7z" fill="#666"/></svg>
       </a>
     </div>
@@ -50,7 +50,8 @@ export default Vue.extend({
   },
 
   data: () =>({
-    txId: '',
+    editingId: '',
+    loadedId: '',
     tags: [] as { name: string, value: string }[],
     loadingTags: false,
     error: '',
@@ -58,29 +59,26 @@ export default Vue.extend({
   }),
 
   created() {
-    this.txId = this.id;
+    this.editingId = this.id;
+    this.loadedId = this.id;
+    this.loadTx(this.loadedId);
   },
 
   watch: {
     '$route': function(val: any) {
       // id is a route prop. 
-      if (this.txId !== this.id) {
-        this.txId = this.id;
-        
+      if (this.loadedId !== this.id) {
+        this.editingId = this.id; 
+        this.loadedId = this.id;
+        this.loadTx(this.loadedId);
       }
     },
-    'txId': function (val: any) {
-      // blerg, this and the above could be made clearer. 
-      // we do this dance with watch and txId / id / router 
-      // so the back button and history is correct, and so we 
-      // are watching for valid pasted in txIds into the input box. 
-      if (this.isTxIdLike(val)) {
-        if (this.id !== this.txId) {
-          this.$router.push({ name: 'tx', params: { id: this.txId }});
-        } else {
-          this.loadTx(this.txId);
+    'editingId': function (val: any) {
+      if (this.isTxIdLike(this.editingId)) {
+        if (this.id !== this.editingId) {
+          this.$router.push({ name: 'tx', params: { id: this.editingId }});
         }
-      } 
+      }
     }
   },
 
